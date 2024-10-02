28f017b68454048725d65d050d68dc31f5790c98
Unnat Malik <160745526+UnnatMalik@users.noreply.github.com>
2024-10-02 19:19:42 +0530

    Removed admin url

--- DBMSwebsite/urls.py
+++ DBMSwebsite/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # create your own admin url 
     path('',include('files.urls'))
 ] 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
